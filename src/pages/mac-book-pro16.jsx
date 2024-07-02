7deeaae05053a4af15dc1bd7da2b82a8e8f43754
KadimiSaiKarthik <KadimiSaiKarthik>
2024-07-02 05:17:46 +0000

    First Commit from Locofy Builder

---
+++ src/pages/mac-book-pro16.jsx
@@ -0,0 +1,273 @@
+import { useCallback } from "react";
+import styles from "./mac-book-pro16.module.css";
+
+const MacBookPro16 = () => {
+  const onMingcuteleftFillClick = useCallback(() => {
+    // Please sync "MacBook Pro 16" - 2" to the project
+  }, []);
+
+  return (
+    <div className={styles.macbookPro165}>
+      <div className={styles.macbookPro165Child} />
+      <div className={styles.macbookPro165Item} />
+      <div className={styles.macbookPro165Inner} />
+      <div className={styles.ellipseDiv} />
+      <div className={styles.macbookPro165Child1} />
+      <div className={styles.macbookPro165Child2} />
+      <main className={styles.staysParent}>
+        <a className={styles.stays}>Stays</a>
+        <a className={styles.flights}>Flights</a>
+        <div className={styles.staysFlightsContent} />
+        <div className={styles.staysFlightsContent1} />
+        <div className={styles.staysFlightsContent2} />
+        <div className={styles.staysFlightsContent3} />
+        <div className={styles.staysFlightsContent4} />
+        <div className={styles.staysFlightsContent5} />
+        <section className={styles.emptyContentParent}>
+          <img
+            className={styles.emptyContentIcon}
+            loading="lazy"
+            alt=""
+            src="/rectangle-10@2x.png"
+          />
+          <img
+            className={styles.emptyContentIcon1}
+            loading="lazy"
+            alt=""
+            src="/rectangle-10@2x.png"
+          />
+          <img
+            className={styles.emptyContentIcon2}
+            loading="lazy"
+            alt=""
+            src="/rectangle-11@2x.png"
+          />
+          <img
+            className={styles.emptyContentIcon3}
+            alt=""
+            src="/rectangle-11@2x.png"
+          />
+          <img
+            className={styles.emptyContentIcon4}
+            loading="lazy"
+            alt=""
+            src="/rectangle-12@2x.png"
+          />
+          <img
+            className={styles.emptyContentIcon5}
+            loading="lazy"
+            alt=""
+            src="/rectangle-12@2x.png"
+          />
+        </section>
+        <img
+          className={styles.mingcuteleftFillIcon}
+          loading="lazy"
+          alt=""
+          src="/mingcuteleftfill.svg"
+          onClick={onMingcuteleftFillClick}
+        />
+        <img
+          className={styles.vectorIcon}
+          loading="lazy"
+          alt=""
+          src="/vector.svg"
+        />
+        <b className={styles.ourTopDestinations}>Our Top Destinations</b>
+        <b className={styles.ourServices}>Our Services</b>
+        <div className={styles.popular}>Popular</div>
+        <div className={styles.curatedExperiences}>Curated Experiences</div>
+        <div className={styles.bestDeals}>Best Deals</div>
+        <img
+          className={styles.iconParkSolidairBike}
+          loading="lazy"
+          alt=""
+          src="/iconparksolidairbike@2x.png"
+        />
+        <img
+          className={styles.streamlinetravelTransportatiIcon}
+          loading="lazy"
+          alt=""
+          src="/streamlinetraveltransportationsailshiptravelboattransportationtransportoceanshipseawater.svg"
+        />
+        <img
+          className={styles.streamlinetravelHotelServinIcon}
+          loading="lazy"
+          alt=""
+          src="/streamlinetravelhotelservingdomecooktooldomekitchendrinkservingplatterdishtoolsfoodcooking.svg"
+        />
+        <img
+          className={styles.humbleiconscar}
+          loading="lazy"
+          alt=""
+          src="/humbleiconscar.svg"
+        />
+        <img
+          className={styles.fluentpersonSupport20FilleIcon}
+          loading="lazy"
+          alt=""
+          src="/fluentpersonsupport20filled.svg"
+        />
+        <img
+          className={styles.majesticonsbeach}
+          loading="lazy"
+          alt=""
+          src="/majesticonsbeach.svg"
+        />
+        <div className={styles.hotels}>Hotels</div>
+        <div className={styles.cars}>Cars</div>
+        <div className={styles.activity}>Activity</div>
+        <div className={styles.fitness}>Fitness</div>
+        <div className={styles.beachs}>Beachs</div>
+        <div className={styles.j24h}>7j/24h</div>
+        <img
+          className={styles.frameChild}
+          loading="lazy"
+          alt=""
+          src="/vector-3.svg"
+        />
+      </main>
+      <img
+        className={styles.pageContentIcon}
+        alt=""
+        src="/page-content@2x.png"
+      />
+      <div className={styles.pageContent} />
+      <div className={styles.wrapperPageContent}>
+        <img
+          className={styles.pageContentIcon1}
+          alt=""
+          src="/page-content@2x.png"
+        />
+      </div>
+      <div className={styles.pageContent1} />
+      <h1 className={styles.exploreTheWorld}>Explore the world with ease</h1>
+      <h3 className={styles.discoverNewCultures}>
+        Discover new cultures and create lifelong memories!
+      </h3>
+      <div className={styles.pageContent2} />
+      <b className={styles.bookNow}>Book Now</b>
+      <img
+        className={styles.simpleIconsyourtraveldottv}
+        loading="lazy"
+        alt=""
+        src="/simpleiconsyourtraveldottv.svg"
+      />
+      <a className={styles.travelor}>Travelor</a>
+      <img
+        className={styles.mingcuteleftFillIcon1}
+        loading="lazy"
+        alt=""
+        src="/mingcuteleftfill-1.svg"
+      />
+      <img
+        className={styles.pageContentIcon2}
+        loading="lazy"
+        alt=""
+        src="/vector-1.svg"
+      />
+      <img className={styles.pageContentIcon3} alt="" src="/page-content.svg" />
+      <div className={styles.pageContent3}>04</div>
+      <div className={styles.pageContent4}>05</div>
+      <h3 className={styles.joinTheBiggest}>
+        Join The Biggest Community Of Travelers
+      </h3>
+      <a className={styles.aboutUs}>About US</a>
+      <div className={styles.weBelieveThat}>
+        we believe that traveling is not just a means of reaching a destination;
+        it is an enriching and transformative experience that opens doors to new
+        cultures, perspectives, and lifelong memories. Our website is designed
+        to be your go-to resource for all things related to travel ...
+      </div>
+      <div className={styles.pageContent5} />
+      <div className={styles.discoverMore}>Discover more</div>
+      <img
+        className={styles.pageContentIcon4}
+        alt=""
+        src="/page-content1@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon5}
+        alt=""
+        src="/page-content2@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon6}
+        alt=""
+        src="/page-content3@2x.png"
+      />
+      <a className={styles.a}>+99</a>
+      <a className={styles.tourCompetion}>Tour Competion</a>
+      <a className={styles.destinationTourism}>Destination Tourism</a>
+      <h1 className={styles.travelor1}>Travelor</h1>
+      <h3 className={styles.travelSmarter}>Travel smarter , not harder</h3>
+      <a className={styles.pageContent6}>+299</a>
+      <a className={styles.pageContent7}>+999</a>
+      <a className={styles.happyCustomer}>Happy Customer</a>
+      <img
+        className={styles.pageContentIcon7}
+        alt=""
+        src="/page-content4@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon8}
+        loading="lazy"
+        alt=""
+        src="/ellipse-8@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon9}
+        loading="lazy"
+        alt=""
+        src="/ellipse-9@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon10}
+        loading="lazy"
+        alt=""
+        src="/ellipse-10@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon11}
+        loading="lazy"
+        alt=""
+        src="/ellipse-11@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon12}
+        loading="lazy"
+        alt=""
+        src="/ellipse-12@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon13}
+        loading="lazy"
+        alt=""
+        src="/ellipse-13@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon14}
+        loading="lazy"
+        alt=""
+        src="/ellipse-14@2x.png"
+      />
+      <img
+        className={styles.pageContentIcon15}
+        loading="lazy"
+        alt=""
+        src="/ellipse-15@2x.png"
+      />
+      <b className={styles.loremIpsumDolor}>
+        Lorem ipsum dolor sit amet consectetur. Tellus interdum placerat quisque
+        diam dui. Quis neque arcu sapien id risus platea viverra suspendisse
+        duis.
+      </b>
+      <button className={styles.joinUsBackgroundParent}>
+        <div className={styles.joinUsBackground} />
+        <div className={styles.joinUs}>Join US</div>
+      </button>
+    </div>
+  );
+};
+
+export default MacBookPro16;
